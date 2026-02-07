51cd871ec2568160cd03e6fc498509ce00739eef
dg2350128-max <dg2350128@gmail.com>
2026-02-07 14:04:06 +0100

    Create mkdirs.bat

---
+++ mkdirs.bat
@@ -0,0 +1,4 @@
+# Create project directories
+mkdir -p "Open Source HackMF Two/app/src/main/java/com/hackmk/opensource"
+mkdir -p "Open Source HackMF Two/app/src/main/res/layout"
+cd "Open Source HackMF Two"
