7eb95a94d81de0009c54896adac46996d9c938f1
dg2350128-max <dg2350128@gmail.com>
2026-02-07 14:03:49 +0100

    Delete mkdirs.cmd

--- mkdirs.cmd
+++
@@ -1,4 +0,0 @@
-# Create project directories
-mkdir -p "Open Source HackMF Two/app/src/main/java/com/hackmk/opensource"
-mkdir -p "Open Source HackMF Two/app/src/main/res/layout"
-cd "Open Source HackMF Two"
